7efac3c0977572bf17b80d3fd3adf01814a8748a
Ricardo Lopez <ricardo.lopez.torres@protonmail.com>
2025-07-08 12:23:13 -0400

    debug macro vk check

--- include/Allocator.hpp
+++ include/Allocator.hpp
@@ -5,22 +5,6 @@
 #include <vma/vk_mem_alloc.h>
 #include <vulkan/vulkan_raii.hpp>
 
-#ifdef NDEBUG
-#define VK_CHECK(vkFnc) vkFnc
-#else
-#include <cassert>
-#include <fmt/base.h>
-#include <vulkan/vk_enum_string_helper.h>
-#define VK_CHECK(vkFnc)                                                                \
-    {                                                                                  \
-        if (const VkResult checkResult = (vkFnc); checkResult != VK_SUCCESS) {         \
-            const char *errMsg = string_VkResult(checkResult);                         \
-            fmt::print(stderr, "Vulkan error: {}", errMsg);                            \
-            assert(checkResult == VK_SUCCESS);                                         \
-        }                                                                              \
-    }
-#endif
-
 /*--
  * A buffer is a region of memory used to store data.
  * It is used to store vertex data, index data, uniform data, and other types of data.
